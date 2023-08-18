d60aa1da14c7c66929f21adb135c1479607686c1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-08-18 18:49:56 +0200

    Finalize the experiment shell

--- submit_hebo.sh
+++ submit_hebo.sh
@@ -6,16 +6,14 @@ echo "Remove failed files"
 echo $cmd
 $cmd
 
-submit_bench () {
+submit_light_bench () {
     fixed_vars_to_use=${1}
 
     declare -A bench_max_id
-    bench_max_id["hpolib"]=3
     bench_max_id["lc"]=33
-    bench_max_id["jahs"]=2
     bench_max_id["hpobench"]=7
 
-    for bench_name in "hpolib" "hpobench" "lc" "jahs"
+    for bench_name in "hpobench" "lc"
     do
         for dataset_id in `seq 0 ${bench_max_id[$bench_name]}`
         do
@@ -35,6 +33,25 @@ submit_bench () {
     done
 }
 
+submit_heavy_bench () {
+    fixed_vars_to_use=${1}
+
+    declare -A bench_max_id
+    bench_max_id["hpolib"]=3
+    bench_max_id["jahs"]=2
+
+    for bench_name in "hpolib" "jahs"
+    do
+        for dataset_id in `seq 0 ${bench_max_id[$bench_name]}`
+        do
+            vars_to_use="${fixed_vars_to_use},BENCH_NAME=${bench_name},DATASET_ID=${dataset_id}"
+            cmd="msub ${vars_to_use} scripts/run_hebo.moab"
+            echo $cmd
+            $cmd
+        done
+    done
+}
+
 for s in 0 1 2
 do
     for n_workers in 1 2 4 8
@@ -42,6 +59,11 @@ do
         seed_start=$(($s * 10))
         seed_end=$(($seed_start + 9))
         fixed_vars_to_use="-v SEED_START=${seed_start},SEED_END=${seed_end},N_WORKERS=${n_workers}"
-        submit_bench "$fixed_vars_to_use"
+        submit_heavy_bench "$fixed_vars_to_use"
+        if [[ "$s" == 0 ]]
+        then
+            fixed_vars_to_use="-v SEED_START=0,SEED_END=29,N_WORKERS=${n_workers}"
+            submit_light_bench "$fixed_vars_to_use"
+        fi
     done
 done
