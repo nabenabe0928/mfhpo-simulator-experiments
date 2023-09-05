ef41f6fe8192d085dc67d646689963d75cfc5933
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-09-06 01:29:54 +0200

    Add hartmann neps

--- submit_neps_jahs.sh
+++ submit_neps_jahs.sh
@@ -23,6 +23,8 @@ do
     done
 done
 
+# 0: cifar10, 1: fashion_mnist, 2: colorectal_histology
+
 cmd="msub run_neps_jahs.moab -l nodes=1:ppn=2,mem=30gb -v SEED=3,N_WORKERS=2,DATASET_ID=1"
 submit_moab "$cmd"
 
@@ -30,4 +32,10 @@ cmd="msub run_neps_jahs.moab -l nodes=1:ppn=2,mem=30gb -v SEED=9,N_WORKERS=2,DAT
 submit_moab "$cmd"
 
 cmd="msub run_neps_jahs.moab -l nodes=1:ppn=4,mem=60gb -v SEED=3,N_WORKERS=4,DATASET_ID=2"
+submit_moab "$cmd"
+
+cmd="msub run_neps_jahs.moab -l nodes=1:ppn=4,mem=60gb -v SEED=11,N_WORKERS=4,DATASET_ID=1"
+submit_moab "$cmd"
+
+cmd="msub run_neps_jahs.moab -l nodes=1:ppn=2,mem=30gb -v SEED=22,N_WORKERS=2,DATASET_ID=1"
 submit_moab "$cmd"
